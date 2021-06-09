b39bf9066920aa7d85d820d842cac590b0927378
Julián Sánchez <j.sanchezfe.2018@alumnos.urjc.es>
2021-06-09 19:00:35 +0200

    Arreglado problema activación entidades en demo5
    El problema estaba en que las entidades se inactivaban transcurrido un
    tiempo, lo he resuelto añadiendo a "ammo-body" la propiedad
    "activationState: disableDeactivation"

--- demos/demo5/component_salta.js
+++ demos/demo5/component_salta.js
@@ -2,8 +2,9 @@ AFRAME.registerComponent('salta',{
     init: function(){
         const el = this.el;
         this.clickListener = function(){
-            const impulse = new Ammo.btVector3(0, 10, 0);
-            const pos = new Ammo.btVector3(0, 0, 0);
+            var position = el.getAttribute('position');
+            var impulse = new Ammo.btVector3(0, 5, 0);
+            var pos = new Ammo.btVector3(position.x, position.y, position.z);
             el.body.applyImpulse(impulse, pos);
             Ammo.destroy(impulse);
             Ammo.destroy(pos);
