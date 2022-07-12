79cdedefe1e3177c665cf6431b6518bad3a9af54
jdjuli <julisanf@gmail.com>
2022-07-12 11:42:41 +0200

    Increased SSL key size
    ToDo: fix deprecation warning "ssl.wrap_socket() is deprecated, use SSLContext.wrap_socket()"

--- run_https.bat
+++ run_https.bat
@@ -1,3 +1,3 @@
 hugo
-openssl.exe req -x509 -newkey rsa:1024 -keyout key.pem -out cert.pem -days 365 -nodes -subj /CN=localhost
+openssl.exe req -x509 -newkey rsa:2048 -keyout key.pem -out cert.pem -days 365 -nodes -subj /CN=localhost
 python ./HTTPS_server.py
